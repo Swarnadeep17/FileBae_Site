284cf21c774f67d90bc68c61e99a38c5e2df2ed4
Swarnadeep17 <diip.brdhn@gmail.com>
2025-05-25 11:40:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Serve static files from 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve index.html for all GET requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.send("Express server for Firebase App Hosting.");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
